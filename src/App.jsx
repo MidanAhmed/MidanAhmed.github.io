f7a9f922f931946db9e7a99f88a82d3cc174a985
unknown <ahmedmidan8@gmail.com>
2024-01-27 00:12:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>👋 Hi, I'm Midan</h2>
+      <h3>I'm a <span className='cta'>Full stack Developer</span> and have completed my B. Tech. in 2023 from North-Eastern Hill University, Shillong. I boast skills like <span className='cta'>ReactJS</span>, <span className='cta'>Next.js</span>, <span className='cta'>Node.js</span> and <span className='cta'>SQL</span>.</h3>
+      <h3>In my leisure time, you'll probably catch me tackling a new language &ndash; and no, I don't mean programming languages this time!</h3 >
+      <p className='read-the-docs'>Website under development</p>
+    </>
+  )
+}
+
+export default App
